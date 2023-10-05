402c413b194c95f59db39263e1f17efdc7393b08
ecampos14 <88864112+ecampos14@users.noreply.github.com>
2023-10-05 20:06:26 -0300

    prova

---
+++ projetolabv-master/projetolabv-master/src/test/java/br/gov/sp/fatec/projetolab5/controller/AnotacaoControllerIntegrationTest.java
@@ -0,0 +1,13 @@
+package br.gov.sp.fatec.projetolab5.controller;
+
+
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+public class AnotacaoControllerIntegrationTest {
+
+ 
+}
+
